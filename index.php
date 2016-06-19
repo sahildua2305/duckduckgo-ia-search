71de451d27101fd8800b0abe6bc0597273febc75
Sahil Dua <sahildua2305@gmail.com>
2016-06-20 00:55:30 +0530

    Add all links to text parameter of result

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * @Author: sahildua2305
  * @Date:   2016-06-19 19:58:20
  * @Last Modified by:   sahildua2305
- * @Last Modified time: 2016-06-20 00:38:11
+ * @Last Modified time: 2016-06-20 00:54:52
  */
 
 header('Content-Type: application/json');
@@ -14,15 +14,14 @@ include 'slack-secrets.php';
 // Verification
 if((!isset($_POST['team_id']) || $_POST['team_id'] !== $TEAM_ID) ||
     (!isset($_POST['token']) || $_POST['token'] !== $TOKEN)) {
-    // die("Invalid request");
-    // echo "Invalid request";
+    die("Invalid request");
+    echo "Invalid request";
 }
 
-// $command = $_POST['command'];
-// $text = $_POST['text'];
-// $token = $_POST['token'];
+$command = $_POST['command'];
+$text = $_POST['text'];
+$token = $_POST['token'];
 
-$text = "url";
 $url = "ia-data.json";
 
 $str = file_get_contents($url);
@@ -46,7 +45,15 @@ foreach($ia_list as $ia) {
     }
 }
 
-print_r($filtered_list);
+$output = "";
+
+foreach($filtered_list as $ia) {
+    $output .= '<a href="https://duck.co/ia/view/' . $ia["id"] . '" target="_blank">';
+    $output .= $ia["name"];
+    $output .= '</a> ';
+    $output .= $ia["description"];
+    $output .= '\n';
+}
 
 $response = array();
 $response["text"] = "This is how search results look like for " . $text;
@@ -55,7 +62,7 @@ $response["attachments"] = array(array());
 $r = array();
 $r["title"] = "DuckDuckGo Search Results";
 $r["title_link"] = "https://duckduckgo.com/?q=" . urlencode($text);
-$r["image_url"] = "http://sahildua.com/img/sahildua.jpg";
+$r["text"] = $output;
 
 $response["attachments"][0] = $r;
 
